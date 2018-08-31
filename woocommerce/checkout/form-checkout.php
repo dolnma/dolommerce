a9bdd51b40a22da00da7a672859186d8b3a24b00
Marek Dolníček <dolnma9@gmail.com>
2018-08-31 14:13:32 +0200

    checkout table order review edit

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -32,32 +32,37 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 ?>
 
-<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
+<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>"
+    enctype="multipart/form-data">
 
 	<?php if ( $checkout->get_checkout_fields() ) : ?>
 
-		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
+	<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
-		<div class="row" id="customer_details">
-			<div class="col-12 col-sm-7 ">
-				<?php do_action( 'woocommerce_checkout_billing' ); ?>
-			</div>
+	<div class="row" id="customer_details">
+		<div class="col-12 col-lg-7 ">
+			<?php do_action( 'woocommerce_checkout_billing' ); ?>
 
-			<div class="col-12 col-sm-5">
-				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
+			<?php do_action( 'woocommerce_checkout_shipping' ); ?>
+
+			<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
+
+			<?php endif; ?>
+		</div>
+
+		<div class="col-12 col-lg-5 checkout__review">
+
+			<h3 class="checkout__review__title">
+				<?php _e( 'Your order', 'understrap' ); ?>
+			</h3>
+
+			<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+
+			<div id="order_review" class="woocommerce-checkout-review-order">
+				<?php do_action( 'woocommerce_checkout_order_review' ); ?>
 			</div>
 		</div>
 
-		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
-
-	<?php endif; ?>
-
-	<h3 id="order_review_heading"><?php _e( 'Your order', 'understrap' ); ?></h3>
-
-	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
-
-	<div id="order_review" class="woocommerce-checkout-review-order">
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
 	</div>
 
 	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
